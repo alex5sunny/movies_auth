0b8685fa6fbde79164c032216ffcc59ecd4b6687
alex5sunny <perekhodovalexey@gmail.com>
2024-10-02 22:22:32 +0300

    start creating db model

---
+++ src/api/users.py
@@ -0,0 +1,36 @@
+from http import HTTPStatus
+from uuid import UUID
+
+from fastapi import APIRouter, Depends
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+router = APIRouter()
+
+
+class UserCreate(BaseModel):
+    login: str
+    password: str
+    first_name: str
+    last_name: str
+
+
+class UserInDB(BaseModel):
+    id: UUID
+    first_name: str
+    last_name: str
+
+    class Config:
+        orm_mode = True
+
+
+@router.post('/signup', response_model=UserInDB, status_code=HTTPStatus.CREATED)
+async def create_user(user_create: UserCreate, db: AsyncSession = Depends(get_session)) -> UserInDB:
+    user_dto = jsonable_encoder(user_create)
+    user = User(**user_dto)
+    db.add(user)
+    await db.commit()
+    await db.refresh(user)
+    return user
+
--- src/db/postgres/PostgresStorage.py
+++ src/db/postgres/PostgresStorage.py
@@ -1,7 +1,10 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine, AsyncSession, async_sessionmaker
+)
+
 from core.config import settings
 from db.Storage import Storage
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from db.postgres.Base import Base
 
 
 class PostgresStorage(Storage):
@@ -11,15 +14,25 @@ class PostgresStorage(Storage):
                     f'{settings.postgres_user}:{settings.postgres_password}@{settings.db_host}:'
                     f'{settings.db_port}/{settings.postgres_db}')
         self.postgres = create_async_engine(self.dsn, echo=True, future=True)
-        self.async_session = sessionmaker(
+        self.async_session = async_sessionmaker(
             self.postgres,
             class_=AsyncSession,
             expire_on_commit=False
         )
 
+    async def __create_schema__(self) -> None:
+        async with self.postgres.begin() as session:
+            await session.run_sync(Base.metadata.create_all)
+
+    async def __purge_database__(self) -> None:
+        async with self.postgres.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+
     async def open(self):
+        # await self.__create_schema__()
         async with self.async_session() as session:
             yield session
 
     async def close(self):
+        # await self.__purge_database__()
         await self.postgres.dispose()
