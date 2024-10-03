89c3d1f0f37a91631eadc79775e2c800bb03c861
Павел Сарыгин <paulssssar@gmail.com>
2024-10-03 20:38:02 +0700

    alembic

---
+++ src/migrate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+
+echo "start migration"
+export DB_DSN="dbname=${DB_NAME} user=${DB_USERNAME} password=${DB_PASSWORD} host=${DB_HOST}"
+alembic upgrade head
+echo "migration complete"
