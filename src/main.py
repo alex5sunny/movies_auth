948cda797db2eccd58f27bbae5f5e55630388a1b
alex5sunny <perekhodovalexey@gmail.com>
2024-10-04 17:44:24 +0300

    соединение с БД, создание таблицы при старте

--- src/main.py
+++ src/main.py
@@ -5,24 +5,27 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
+from api import users
 from core.config import settings
 from core.logger import LOGGING
 from db import db_cache
 from db import db_storage
-from db.redis.RedisCache import RedisCache
 from db.postgres.PostgresStorage import PostgresStorage
+from db.redis.RedisCache import RedisCache
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     db_cache.cache = RedisCache(settings.redis_host, settings.redis_port)
     db_storage.storage = PostgresStorage()
-    db_storage.storage.open()
+    from models.entity import User  # necessary to create table
+    await db_storage.storage.create_schema()
 
     yield
 
     await db_cache.cache.close()
     if db_storage.storage:
+        await db_storage.storage.purge_schema()
         await db_storage.storage.close()
 
 
