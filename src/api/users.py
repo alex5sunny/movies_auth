a41b80baae6b36a7dc7e9922e68b47c2e164e690
Павел Сарыгин <paulssssar@gmail.com>
2024-10-04 20:12:21 +0700

    213. Добавлена в transaction.atomic установка даты следующего списания

--- src/api/users.py
+++ src/api/users.py
@@ -28,9 +28,23 @@ class UserInDB(BaseModel):
 @router.post('/signup', response_model=UserInDB, status_code=HTTPStatus.CREATED)
 async def create_user(user_create: UserCreate, db: AsyncSession = Depends(get_session)) -> UserInDB:
     user_dto = jsonable_encoder(user_create)
-    user = User(**user_dto)
+    user = UserInDB(**user_dto)
     db.add(user)
     await db.commit()
     await db.refresh(user)
     return user
 
+
+@router.get(
+    path='/signin',
+    summary="Login",
+    description="Login by email and password."
+)
+async def login_user(
+        email: str,
+        password: str,
+        user_device_type: DeviceType = Query(..., alias="user_device_type"),
+        service: UserService = Depends(get_user_service)
+) -> Response:
+    response: dict = await service.authenticate(email, password, user_device_type)
+    return get_tokens_response(response)
