30dde0c22d5ab515ea8b8c834e5f95cc60002b7f
Aman Turakhanov <C:\mail>
2024-10-15 00:35:53 +0300

    refactoring and bugs fixing

--- src/api/roles.py
+++ src/api/roles.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
-from typing import Optional
+from typing import Optional, List
+from uuid import UUID
+
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 
@@ -15,7 +17,7 @@ class RoleCreate(BaseModel):
 
 
 class RoleInDB(BaseModel):
-    id: int
+    id: UUID
     name: str
     description: Optional[str] = None
 
@@ -27,19 +29,15 @@ class RoleInDB(BaseModel):
 @superuser_required
 async def create_role(
     role: RoleCreate,
-    role_service: RoleService = Depends(get_role_service)
+    role_service: RoleService = Depends(get_role_service),
 ):
-    try:
-        new_role = await role_service.create_role(name=role.name, description=role.description)
-        return new_role
-    except HTTPException as e:
-        raise HTTPException(status_code=e.status_code, detail=e.detail)
+    new_role = await role_service.create_role(name=role.name, description=role.description)
+    return new_role
 
 
-
-@router.get("/roles/", response_model=list[RoleInDB])
+@router.get("/roles/", response_model=List[RoleInDB])
 async def get_roles(
-    role_service: RoleService = Depends(get_role_service)
+    role_service: RoleService = Depends(get_role_service),
 ):
     roles = await role_service.get_roles()
     return roles
@@ -47,8 +45,8 @@ async def get_roles(
 
 @router.get("/roles/{role_id}", response_model=RoleInDB)
 async def get_role(
-    role_id: int,
-    role_service: RoleService = Depends(get_role_service)
+    role_id: UUID,
+    role_service: RoleService = Depends(get_role_service),
 ):
     role = await role_service.get_role(role_id=role_id)
     if not role:
@@ -62,28 +60,18 @@ async def get_role(
 @router.put("/roles/{role_id}", response_model=RoleInDB)
 @superuser_required
 async def update_role(
-    role_id: int,
+    role_id: UUID,
     role: RoleCreate,
-    role_service: RoleService = Depends(get_role_service)
+    role_service: RoleService = Depends(get_role_service),
 ):
     updated_role = await role_service.update_role(role_id=role_id, name=role.name, description=role.description)
-    if not updated_role:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail="Role not found."
-        )
     return updated_role
 
 
 @router.delete("/roles/{role_id}", status_code=HTTPStatus.NO_CONTENT)
 @superuser_required
 async def delete_role(
-    role_id: int,
-    role_service: RoleService = Depends(get_role_service)
+    role_id: UUID,
+    role_service: RoleService = Depends(get_role_service),
 ):
-    deleted = await role_service.delete_role(role_id=role_id)
-    if not deleted:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail="Role not found."
-        )
+    await role_service.delete_role(role_id=role_id)
