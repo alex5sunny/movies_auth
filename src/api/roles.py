12f90ace380deef0d28177ef3b726df55a6ed107
Aman Turakhanov <C:\mail>
2024-10-14 17:42:01 +0300

    add superuser decorator

--- src/api/roles.py
+++ src/api/roles.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 
 from services.roles import RoleService, get_role_service
+from src.services.decorators import superuser_required
 
 router = APIRouter()
 
@@ -23,6 +24,7 @@ class RoleInDB(BaseModel):
 
 
 @router.post("/roles/", response_model=RoleInDB, status_code=HTTPStatus.CREATED)
+@superuser_required
 async def create_role(
     role: RoleCreate,
     role_service: RoleService = Depends(get_role_service)
@@ -58,6 +60,7 @@ async def get_role(
 
 
 @router.put("/roles/{role_id}", response_model=RoleInDB)
+@superuser_required
 async def update_role(
     role_id: int,
     role: RoleCreate,
@@ -73,6 +76,7 @@ async def update_role(
 
 
 @router.delete("/roles/{role_id}", status_code=HTTPStatus.NO_CONTENT)
+@superuser_required
 async def delete_role(
     role_id: int,
     role_service: RoleService = Depends(get_role_service)
